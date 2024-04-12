a81cdaedf6b7be450e93f674827f3c31c6e51554
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-04-12 01:28:41 +0000

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -80,6 +80,7 @@ def get_logger() -> logging.Logger:
     logger.setLevel(logging.INFO)
     formatter = RedactingFormatter(fields=PII_FIELDS)
     stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
