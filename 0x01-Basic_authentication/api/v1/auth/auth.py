62b5ddee44ba2bd2a30ba5a68344419039426f5a
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-04-18 00:58:53 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+The class to manage the API authentication
+"""
 
 from typing import List, TypeVar
 from flask import request
@@ -18,7 +20,11 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """
@@ -31,7 +37,11 @@ class Auth:
         Returns:
             str: The authorization header value.
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
