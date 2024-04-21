9dc3a6ceb742bf18dc1c4b8d181e9db96ca698a4
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-04-21 13:22:11 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,11 +58,13 @@ def before_request() -> None:
         '/api/v1/status',
         '/api/v1/status/',
         '/api/v1/unauthorized',
-        '/api/v1/forbidden'
+        '/api/v1/forbidden',
+        '/api/v1/auth_session/login/'
     ]
 
     if request.path not in excluded_paths:
-        if auth.authorization_header(request) is None:
+        if (auth.authorization_header(request) is None
+                and auth.session_cookie(request) is None):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
